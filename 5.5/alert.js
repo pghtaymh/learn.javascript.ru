f68471a10d343658acb9f7943ccf15d6bf638e48
Akella <walx404@gmail.com>
2020-03-12 21:31:17 +0300

    add

--- 5.5/alert.js
+++ 5.5/alert.js
@@ -83,4 +83,107 @@ let arr = [5, 2, 1, -10, 8];
 // ... ваш код для сортировки по убыванию
 
 alert( arr ); // 8, 5, 2, 1, -10
+
+let arr = [5, 2, 1, -10, 8];
+arr.sort( (a, b) => a - b);
+console.log(arr);
+*/
+
+/*
+У нас есть массив строк arr. Нужно получить отсортированную копию, но оставить arr неизменённым.
+
+Создайте функцию copySorted(arr), которая будет возвращать такую копию.
+
+let arr = ["HTML", "JavaScript", "CSS"];
+
+let sorted = copySorted(arr);
+
+alert( sorted ); // CSS, HTML, JavaScript
+alert( arr ); // HTML, JavaScript, CSS (без изменений)
+
+function copySorted(arr) {
+    let newArray = arr.slice();
+    return newArray.sort();
+
+}
+let arr = ["HTML", "JavaScript", "CSS"];
+
+let sorted = copySorted(arr);
+console.log(arr);
+console.log(sorted);
+*/
+
+/*
+Создайте функцию конструктор Calculator, которая создаёт «расширяемые» объекты калькулятора.
+
+Задание состоит из двух частей.
+
+Во-первых, реализуйте метод calculate(str), который принимает строку типа "1 + 2" в формате «ЧИСЛО оператор ЧИСЛО» (разделено пробелами) и возвращает результат. Метод должен понимать плюс + и минус -.
+
+Пример использования:
+
+let calc = new Calculator;
+
+alert( calc.calculate("3 + 7") ); // 10
+Затем добавьте метод addMethod(name, func), который добавляет в калькулятор новые операции. Он принимает оператор name и функцию с двумя аргументами func(a,b), которая описывает его.
+
+Например, давайте добавим умножение *, деление / и возведение в степень **:
+
+let powerCalc = new Calculator;
+powerCalc.addMethod("*", (a, b) => a * b);
+powerCalc.addMethod("/", (a, b) => a / b);
+powerCalc.addMethod("**", (a, b) => a ** b);
+
+let result = powerCalc.calculate("2 ** 3");
+alert( result ); // 8
+Для этой задачи не нужны скобки или сложные выражения.
+Числа и оператор разделены ровно одним пробелом.
+Не лишним будет добавить обработку ошибок.
+
+function Calculator() {
+    this.Methods = {
+        '+': (a, b) => a + b,
+        '-': (a, b) => a - b
+    }
+    this.calculate = function (str) {
+        let array = str.split(' ');
+        let op = array[1];
+        let operandOne = +array[0];
+        let operandTwo = +array[2];
+        
+        if (isNaN(operandOne) || isNaN (operandTwo) || this.Methods[op] == null) {
+            return null;
+        }
+        else return this.Methods[op](operandOne,operandTwo);
+        
+    }
+    this.addMethod = function(name, func) {
+        this.Methods[name] = func;
+    }
+}
+let calculator = new Calculator();
+let oneOperation = calculator.calculate("1 + 2");
+let twoOperation = calculator.calculate("1 - 2");
+console.log(oneOperation);
+console.log(twoOperation);
+calculator.addMethod("**", (a, b) => a ** b);
+let result = calculator.calculate("2 ** 3");
+console.log(result); // 8
+console.log(calculator.Methods);
+*/
+
+/*
+У вас есть массив объектов user, и в каждом из них есть user.name. Напишите код, который преобразует их в массив имён.
+
+Например:
+
+let vasya = { name: "Вася", age: 25 };
+let petya = { name: "Петя", age: 30 };
+let masha = { name: "Маша", age: 28 };
+
+let users = [ vasya, petya, masha ];
+
+let names = /* ... ваш код */
+
+alert( names ); // Вася, Петя, Маша
 */
